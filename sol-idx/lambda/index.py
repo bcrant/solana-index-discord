fb190c08448fce16f686a001ea2d716bee6569cd
Brian Crant <bcrant@gmail.com>
2022-01-17 11:34:06 -0700

    Send response to Discord.

--- sol-idx/lambda/index.py
+++ sol-idx/lambda/index.py
@@ -1,6 +1,7 @@
 import json
 import os
 import pprint
+import requests
 from dotenv import load_dotenv
 from nacl.signing import VerifyKey
 from nacl.exceptions import BadSignatureError
@@ -20,6 +21,9 @@ def handler(event, context):
     PUBLIC_KEY = os.getenv('DISCORD_PUBLIC_KEY')
     verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
     logger.info(f'Verify Key: {verify_key}')
+    #
+    # headers = event.get('headers')
+    # logger.info(f'Headers: {headers}')
 
     signature = event.get('signature')
     logger.info(f'Signature: {signature}')
@@ -36,16 +40,37 @@ def handler(event, context):
         logger.info(f'Verify Result: {verify_result}')
 
         if body.get('type') == 1:
-            return {
-                'statusCode': 200,
-                'body': json.dumps({'type': 1})
-            }
+            v = validate_response(body)
+            return logger.info(f'Validation Response: {v.content}')
+
     except BadSignatureError as e:
         logger.error(f'Bad Signature Error: {e}')
         return {
             'statusCode': 401,
             'body': json.dumps({'err': 'Bad Signature Error'})
         }
+
+
+def validate_response(req_body):
+
+    interaction_id = req_body.get('id')
+    interaction_token = req_body.get('token')
+
+    url = f'https://discord.com/api/v8/interactions' \
+          f'/{interaction_id}' \
+          f'/{interaction_token}' \
+          f'/callback'
+    # response = json.dumps({
+    #     'statusCode': 200,
+    #     'type': 1,
+    # })
+    response = {
+        'statusCode': 200,
+        'body': json.dumps({'type': 1})
+    }
+    r = requests.post(url, json=response)
+
+    return r
 
 
 if os.getenv('AWS_EXECUTION_ENV') is None:
