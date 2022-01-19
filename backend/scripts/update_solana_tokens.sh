6c4decc712fc346cbed38c7b56e51a925be2fd73
Brian Crant <bcrant@gmail.com>
2022-01-18 18:43:46 -0700

    Remove old repo structure.

--- backend/scripts/update_solana_tokens.sh
+++
@@ -1,9 +0,0 @@
-#
-# Update List of Solana Tokens
-#
-curl https://raw.githubusercontent.com/solana-labs/token-list/main/src/tokens/solana.tokenlist.json \
-  | jq \
-  | grep "\"symbol\": " \
-  | sort \
-  > solana_tokens.txt \
-  && sed -i "" "s|^      \"symbol\": ||" ./solana_tokens.txt
