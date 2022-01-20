e0f8c306403a52c1025e932dd8038cdbd40568bc
Brian Crant <bcrant@everfi.com>
2022-01-20 13:13:32 -0700

    Add stubs to fix remote Lambda imports.

--- sol-idx/lambda/index.py
+++ sol-idx/lambda/index.py
@@ -1,7 +1,13 @@
 import os
+import sys
 from utils.discord.interactions import validate_discord_interaction
 from utils.environment import init_env
-from utils.pyth.pyth import get_pyth_price_feed
+print('os.getcwd: ', os.getcwd())
+print('sys.path: ', sys.path)
+print('os.path.join(os.path.dirname(__file__)): ', os.path.join(os.path.dirname(__file__)))
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+sys.path.append('./utils/')
+print('sys.path: ', sys.path)
 
 
 def handler(event, context):
@@ -13,9 +19,7 @@ def handler(event, context):
     logger.info(f'[EVENT]: {event}')
     logger.info(f'[CONTEXT]: {context}')
 
-    discord_msg = get_pyth_price_feed()
-
-    validate_discord_interaction(event, discord_msg, logger)
+    validate_discord_interaction(event, logger)
 
 
 if os.getenv('AWS_EXECUTION_ENV') is None:
